1e7c4b921722c16b6df8b285edc77139621b4acf
Dobroslav Svyatovid <vasnomedas@gmail.com>
2014-03-16 12:21:07 +0200

    Added unpack+kw

--- common/table.lua
+++ common/table.lua
@@ -10,8 +10,72 @@ function table_module.index(tbl, value)
     return nil
 end
 
+-- Just a handy functin for printing tables
 function table_module.tostring(tbl)
     return '{' .. table.concat(tbl, ', ') .. '}'
+end
+
+-- Update tbl contents with tables in ...
+function table_module.update(tbl, ...)
+    for i = 1, select('#', ...) do
+        for k, v in pairs(select(i, ...)) do
+            tbl[k] = v
+        end
+    end
+    return tbl
+end
+
+-- Used by vararg_to_table to denote keyword arguments
+local kw_meta = {}
+
+function table_module.kw(tbl)
+    return setmetatable({value=tbl}, kw_meta)
+end
+
+function table_module.is_kw(tbl)
+    return getmetatable(tbl) == kw_meta
+end
+
+-- Used to make vararg_to_table idempotent
+local unpacked_vararg_meta = {}
+
+function table_module.unpacked_vararg(tbl)
+    return setmetatable(tbl, unpacked_vararg_meta)
+end
+
+function table_module.is_unpacked_vararg(tbl)
+    return getmetatable(tbl) == unpacked_vararg_meta
+end
+
+-- Similar to table.unpack but with support of keyword arguments
+-- f(41, nil, 43, {value=42}, kw{a=1, b=2}, kw{c=3}) ->
+--      {[1] = 41, [2] = nil, [3] = 43, [4] = {value=42}, kw={a=1, b=2, c=3}}
+-- f(f(...)) == f(...) so this function can be applied to the same result
+-- multiple times
+-- However, f(f(...), f(...)) will turn into a sequence with two elements.
+function table_module.vararg_to_table(...)
+    local result = {}
+    local length = select('#', ...)
+    if length == 0 then
+        -- pass
+    elseif length == 1 and table_module.is_unpacked_vararg(select(1, ...)) then
+        result = select(1, ...)
+    else
+        for i = 1, length do
+            local obj = select(i, ...)
+            if table_module.is_kw(obj) then
+                -- Merge keywords into result.kw
+                result.kw = result.kw or {}
+                table_module.update(result.kw, obj.value)
+            else
+                -- Other arguments are just elements of array
+                local insert_index = (result.n or 0) + 1
+                result[insert_index] = obj
+                result.n = insert_index
+            end
+        end
+    end
+    return table_module.unpacked_vararg(result)
 end
 
 return table_module
--- tests/test_table.lua
+++ tests/test_table.lua
@@ -4,6 +4,10 @@ local table_m = common.table
 
 module( "test_table", package.seeall, lunit.testcase )
 
+-- function assert_tbl_equal(tbl1, tbl2)
+--     for 
+-- end
+
 function test_index()
     index = table_m.index
     tbl = {1, 2, 3, 4, 6, 2}
@@ -19,4 +23,94 @@ function test_tostring()
     assert_equal('{1, 2, 3}', tostr{1, 2, 3})
     assert_equal('{}', tostr{})
     -- TODO: String repr
+end
+
+function test_kw()
+    tbl = {}
+    kwtbl = table_m.kw(tbl)
+    assert_equal(tbl, kwtbl.value)
+    assert_true(table_m.is_kw(kwtbl))
+    assert_false(table_m.is_kw(tbl))
+end
+
+function test_unpacked_vararg_check()
+    tbl = {}
+    vatbl = table_m.unpacked_vararg(tbl)
+    assert_equal(tbl, vatbl)
+    assert_true(table_m.is_unpacked_vararg(vatbl))
+    assert_false(table_m.is_unpacked_vararg({}))
+end
+
+function test_update()
+    tbl = {}
+    assert_equal(tbl, table_m.update(tbl))
+    tbl2 = table_m.update(tbl, {a=1,b=2}, {c=3})
+    assert_equal(tbl, tbl2)
+    assert_equal(1, tbl.a)
+    assert_equal(2, tbl.b)
+    assert_equal(3, tbl.c)
+end
+
+function test_vararg_to_table()
+    vtt = table_m.vararg_to_table
+    tbl = vtt()
+    assert_equal('{}', table_m.tostring(tbl))
+    assert_true(table_m.is_unpacked_vararg(tbl))
+
+    tbl2 = vtt(tbl)
+    assert_equal(tbl2, tbl)
+    assert_true(table_m.is_unpacked_vararg(tbl2))
+
+    tbl3 = vtt(1, 2, 3)
+    assert_equal(3, tbl3.n)
+    for i = 1,tbl3.n do
+        assert_equal(i, tbl3[i])
+    end
+
+    sometbl = {a=1}
+    tbl4 = vtt(sometbl)
+    assert_equal(1, tbl4.n)
+    assert_equal(sometbl, tbl4[1])
+
+    kw = table_m.kw
+
+    tbl5 = vtt("hello", kw{a=1, b=2})
+    assert_equal(1, tbl5.kw.a)
+    assert_equal(2,tbl5.kw.b)
+    assert_equal(1, tbl5.n)
+    assert_equal("hello", tbl5[1])
+
+    tbl6 = vtt("a", "b", kw{exc_info=true, log_exc=true}, kw{exc_info=false})
+    assert_false(tbl6.kw.exc_info)
+    assert_true(tbl6.kw.log_exc)
+
+    tbl7 = vtt(nil)
+    assert_equal(1, tbl7.n)
+    assert_nil(tbl7[1])
+
+    valtbl = {value=42}
+    tbl8 = vtt(nil, valtbl, nil, kw{a=1})
+    assert_equal(3, tbl8.n)
+    assert_nil(tbl8[1])
+    assert_equal(valtbl, tbl8[2])
+    assert_nil(tbl8[3])
+    assert_equal(1, tbl8.kw.a)
+
+    tbl9a = vtt(1,2,3)
+    tbl9b = vtt(4,5,6)
+    assert_equal(tbl9a, vtt(tbl9a))
+    tbl9c = vtt(tbl9a, tbl9b)
+    assert_equal(tbl9a, tbl9c[1])
+    assert_equal(tbl9b, tbl9c[2])
+
+    -- Docstring example
+    tbldoc = vtt(41, nil, 43, {value=42}, kw{a=1, b=2}, kw{c=3})
+    assert_equal(41, tbldoc[1])
+    assert_equal(nil, tbldoc[2])
+    assert_equal(43, tbldoc[3])
+    assert_equal(42, tbldoc[4].value)
+    assert_equal(1, tbldoc.kw.a)
+    assert_equal(2, tbldoc.kw.b)
+    assert_equal(3, tbldoc.kw.c)
+
 end
